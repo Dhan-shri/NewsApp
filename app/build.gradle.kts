777481ec89745c69847f61dd4dfc0da61fe71f05
Dhanshri <dhanshri20@navgurukul.org>
2024-01-19 23:11:49 +0530

    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,4 +87,11 @@ dependencies {
 
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.sqareup.moshi:moshi-kotlin:1.12.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
 }
